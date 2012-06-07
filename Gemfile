929ea15f32be4749ec1e9d22702c97cbb6897b3c
Sergey Rezvanov <sergey@rezvanov.info>
2012-06-07 22:32:49 +0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rake', '0.9.2.2'
+gem 'unicorn'
+gem 'mysql2', '0.2.6'
+gem 'rails', '3.2.5'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appletunity::Application.config.secret_token = '20243a613ef7e0bf096ebc43497744c7905b1ed8ba88cbd12da92ef2d816cb2987373e0a4dfc2a3f79dcc60766a26add103a1db38bebaf8380dbbb4c01c81ef7'
