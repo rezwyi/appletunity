4539867e6b7412fb3d94e6abed727ea665674f10
Sergey Rezvanov <sergey@rezvanov.info>
2012-08-30 11:43:31 +0600

    Removed unused specs

--- spec/models/city_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe City do
-  it { should_not be_valid }
-  it { should validate_presence_of :name }
-end
--- spec/models/company_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe Company do
-  it { should_not be_valid }
-  it { should validate_presence_of :name }
-end
