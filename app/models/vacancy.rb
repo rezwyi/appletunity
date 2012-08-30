01494522ddd9fa539233a7090b0e9f96bec6e969
Sergey Rezvanov <sergey@rezvanov.info>
2012-08-30 11:42:20 +0600

    Added autofill expired_at and edit_token

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -8,4 +8,26 @@ class Vacancy < ActiveRecord::Base
   attr_accessible :company_name, :company_website, :title, :description,
                   :location, :occupations, :contact_email, :contact_phone,
                   :agreed_to_offer
+
+  before_save :generate_expired_at
+  before_save :generate_edit_token
+
+  # Simple random token generator
+  def self.friendly_token
+    SecureRandom.base64(15).tr('+/=', '-_ ').strip.delete("\n")
+  end
+
+  protected
+
+  def generate_expired_at
+    self.expired_at = Time.now + 7.days
+  end
+
+  def generate_edit_token
+    token = Vacancy.friendly_token
+    while Vacancy.find(:first, :conditions => {:edit_token => token})
+      token = Vacancy.friendly_token
+    end
+    self.edit_token = token
+  end
 end
--- spec/models/vacancy_spec.rb
+++ spec/models/vacancy_spec.rb
@@ -6,4 +6,11 @@ describe Vacancy do
   it { should validate_presence_of :description }
   it { should validate_presence_of :contact_email }
   it { should validate_presence_of :agreed_to_offer }
+
+  context '#self.friendly_token' do
+    it 'should return token' do
+      SecureRandom.stub(:base64).and_return("+A/BC=\n")
+      Vacancy.friendly_token.should eq '-A_BC'
+    end
+  end
 end
