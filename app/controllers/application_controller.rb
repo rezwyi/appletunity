61c265abd14041860513903f2a8eb51401798893
Sergey Rezvanov <sergey@rezvanov.info>
2012-09-01 21:42:29 +0600

    Added render_404 method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def render_404
+    respond_to do |type|
+      type.html do
+        render :status => :not_found, :layout => false,
+               :file => "#{Rails.root}/public/404.html"
+      end
+      type.all { render :status => :not_found, :nothing => true }
+    end
+  end
 end
