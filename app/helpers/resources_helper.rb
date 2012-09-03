9c72f4cbec0355f09665c049ba9fb59d96f3a237
Sergey Rezvanov <sergey@rezvanov.info>
2012-09-04 00:50:51 +0600

    Refactored edit and delete resource links

---
+++ app/helpers/resources_helper.rb
@@ -0,0 +1,40 @@
+module ResourcesHelper
+  include Rails.application.routes.url_helpers
+
+  # Public: Build link to edit resource (e.g. admin, vacancy)
+  #
+  # resource - Resource object (required)
+  # options - The options Hash (default: {})
+  #
+  # Examples
+  #
+  #   link_to_edit_resource(vacancy)
+  #
+  # Returns html safe String
+  def link_to_edit_resource(resource, options = {})
+    return unless resource
+    
+    link_to t('edit'),
+            polymorphic_path([:administration, resource], :action => :edit),
+            options
+  end
+  
+  # Public: Build link to delete resource (e.g. admin, vacancy)
+  #
+  # resource - Resource object (required)
+  # options - The options Hash (default: {})
+  #
+  # Examples
+  #
+  #   link_to_delete_resource(vacancy)
+  #
+  # Returns html safe String
+  def link_to_delete_resource(resource, options = {})
+    return unless resource
+    
+    defaults = {:method => :delete, :data => {:confirm => 'Are you sure?'}}
+    options = options.merge(defaults)
+    
+    link_to t('delete'), polymorphic_path([:administration, resource]), options
+  end
+end
---
+++ spec/helpers/resources_helper_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe VacanciesHelper do
+  before :each do
+    
+  end
+
+  describe '#link_to_edit_resource' do
+    it 'should return nil' do
+      helper.link_to_edit_resource(nil).should be_nil
+    end
+
+    it 'should return correct link' do
+      vacancy = mock_model(Vacancy)
+      vacancy.stub(:id).and_return(1)
+      helper.link_to_edit_resource(vacancy)\
+            .start_with?(%(<a href="/administration/vacancies/1/edit">))\
+            .should be_true
+    end
+  end
+
+   describe '#link_to_delete_resource' do
+    it 'should return nil' do
+      helper.link_to_delete_resource(nil).should be_nil
+    end
+  end
+end
