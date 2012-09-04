24291c265e8fbfa864c90aab6b6e97a7c17ccff9
Sergey Rezvanov <sergey@rezvanov.info>
2012-09-04 11:48:41 +0600

    Setting up action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Appletunity::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -34,4 +34,11 @@ Appletunity::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Action Mailer and Mailcatcher
+  ActionMailer::Base.default :from => 'test@appletunity.ru'
+  config.action_mailer.default_url_options = {:host => 'localhost:8888'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'localhost',
+                                         :port => 1025 }
 end
