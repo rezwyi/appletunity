8a92d3d82759eab57e8a248d6bbae121b25ef6f9
Sergey Rezvanov <sergey@rezvanov.info>
2012-09-12 23:09:20 +0600

    404 if trying to show vacancies whos not approved

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -24,7 +24,7 @@ class VacanciesController < ApplicationController
   end
 
   def show
-    render_404 and return if @vacancy.expired?
+    render_404 and return if @vacancy.expired? || !@vacancy.approved?
   end
 
   def edit
