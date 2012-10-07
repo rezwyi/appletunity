2adb363f549b795c4cc3b57e1e5d5ccab6004786
Sergey Rezvanov <sergey@rezvanov.info>
2012-10-08 01:19:22 +0600

    Added alt attribute for all logos

--- spec/helpers/vacancies_helper_spec.rb
+++ spec/helpers/vacancies_helper_spec.rb
@@ -41,24 +41,25 @@ describe VacanciesHelper do
     context 'when Vacancy instance given' do
       before :each do
         @vacancy = mock_model(Vacancy)
+        @vacancy.stub(:company_name).and_return('some_company_name')
         @vacancy.stub_chain(:logo, :url).and_return('/assets/some_logo.png')
       end
       
       it 'should return no logo image tag' do
         @vacancy.stub(:logo?).and_return(false)
         helper.logo_for(@vacancy)\
-              .should eq %(<img alt="No_logo" src="/assets/no_logo.png" />)
+              .should eq %(<img alt="some_company_name" src="/assets/no_logo.png" />)
       end
 
       it 'should return vacancy logo image tag' do
         @vacancy.stub(:logo?).and_return(true)
         helper.logo_for(@vacancy)\
-              .should eq %(<img alt="Some_logo" src="/assets/some_logo.png" />)
+              .should eq %(<img alt="some_company_name" src="/assets/some_logo.png" />)
       end
 
       it 'should return vacancy logo image tag with class' do
         @vacancy.stub(:logo?).and_return(true)
-        s = %(<img alt="Some_logo" class="well" src="/assets/some_logo.png" />)
+        s = %(<img alt="some_company_name" class="well" src="/assets/some_logo.png" />)
         helper.logo_for(@vacancy, :medium, :class => 'well').should eq s
       end
     end
