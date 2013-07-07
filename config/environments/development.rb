bc93185f549d8aff42c2dd9f9e21004639bfff50
Sergey Rezvanov <sergey@rezvanov.info>
2013-07-07 18:39:12 +0600

    Disable raise error on failed email sending in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Appletunity::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -39,8 +39,7 @@ Appletunity::Application.configure do
   ActionMailer::Base.default :from => 'test@appletunity.ru'
   config.action_mailer.default_url_options = {:host => 'localhost:8888'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'localhost',
-                                         :port => 1025 }
+  config.action_mailer.smtp_settings = {:address => 'localhost', :port => 1025 }
 
   # Default host
   config.after_initialize do
