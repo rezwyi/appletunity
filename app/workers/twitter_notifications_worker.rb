ec977cae9022458b29e892a5b79fbbc6fb252536
Sergey Rezvanov <sergey@rezvanov.info>
2013-07-14 20:52:11 +0600

    Fix bug with twitter worker

--- app/workers/twitter_notifications_worker.rb
+++ app/workers/twitter_notifications_worker.rb
@@ -3,12 +3,6 @@ class TwitterNotificationsWorker
   
   def self.perform(id)
     return unless (v = Vacancy.find_by_id(id))
-    Twitter.update tweet_text(v)
-  end
-
-  protected
-
-  def tweet_text(vacancy)
-    "[#{v.company_name}] #{v.title} #{Rails.application.routes.url_helpers.vacancy_url(v.id)} #appletunity"
+    Twitter.update "[#{v.company_name}] #{v.title} #{Rails.application.routes.url_helpers.vacancy_url(v.id)} #appletunity"
   end
 end
