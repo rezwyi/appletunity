f36bfab5a382f2295b4903642f737066cd0d3f7d
Sergey Rezvanov <sergey@rezvanov.info>
2013-07-14 17:40:04 +0600

    Configure redis

---
+++ config/initializers/redis.rb
@@ -0,0 +1,9 @@
+module Rails
+  def self.redis
+    @_redis ||= {}
+    @_redis[Rails.env] ||= Redis::Namespace.new(
+      "appeltunity:#{Rails.env}",
+      :redis => Redis.new(:host => 'localhost', :port => '6379')
+    )
+  end
+end
