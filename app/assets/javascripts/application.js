310ffebd770afff66d58383a7e6749f1466d0e5a
Sergey Rezvanov <sergey@rezvanov.info>
2014-02-10 14:16:14 +0600

    Remove unnecessary plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require flight-for-rails
-//= require plugins/jquery.cookie
 //= require plugins/redactor
 //= require plugins/redactor.locale
 //= require components/application
