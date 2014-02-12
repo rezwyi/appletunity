cc52d3b8a4f5f3f3ec34e1379968f1fad9db381d
Sergey Rezvanov <sergey@rezvanov.info>
2014-02-12 17:03:08 +0600

    Fix twitter worker

--- app/workers/twitter_notifications_worker.rb
+++ app/workers/twitter_notifications_worker.rb
@@ -1,16 +1,18 @@
 class TwitterNotificationsWorker
-  include Rails.application.routes.url_helpers
-  
   @queue = :notifications
   
   def self.perform(id)
     return unless (v = Vacancy.find_by_id(id))
-    twitter_client.update "[#{v.company_name}] #{v.title} #{vacancy_url(v)} #appletunity"
+    twitter_client.update "[#{v.company_name}] #{v.title} #{Rails.application.routes.url_helpers.vacancy_url(v, host: host)} #appletunity"
   end
 
   protected
 
-  def twitter_client
-    @twitter_client ||= Twitter::REST::Client.new(Settings.credentials.twitter)
+  def self.twitter_client
+    Twitter::REST::Client.new Settings.credentials.twitter.to_h
+  end
+
+  def self.host
+  	ActionMailer::Base.default_url_options[:host]
   end
 end
